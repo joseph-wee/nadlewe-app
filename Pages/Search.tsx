06dcac3e6711f8e7f189bb10279f10fd5c6740d8
황현정 <ghkd2271@naver.com>
2023-11-18 22:50:36 +0900

    feat: 코스 수 추가기능 구현
    기능 자체는 구현했고, 버튼 디자인 바꾼후 밑에 카테고리부분 추가하면 됨.

--- Pages/Search.tsx
+++ Pages/Search.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet,TouchableOpacity,Image  } from 'react-native';
+import { View, Text,FlatList, StyleSheet,TouchableOpacity,Image,ImageSourcePropType   } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 import MultiSlider from '@ptomasroos/react-native-multi-slider'; // Ensure this package is installed
 import { Picker } from '@react-native-picker/picker';
@@ -7,66 +7,58 @@ import { Picker } from '@react-native-picker/picker';
 type SearchProps = {
   navigation: any; // You can define a more specific type for navigation if needed
 };
-
+type Tag = {
+  key: string;
+  icon: ImageSourcePropType;
+};
+type SelectedTag = Tag & { id: string };
+const tagsData: Tag[] = [
+  { key: '식사', icon: require('../assets/icons/icon_Utensils_w.png') },
+  { key: '카페', icon: require('../assets/icons/icon_MugHot_w.png') },
+  { key: '활동', icon: require('../assets/icons/icon_Running_w.png') },
+  { key: '숙소', icon: require('../assets/icons/icon_Bed_w.png') },
+];
 const Search: React.FC<SearchProps> = ({ navigation }) => {
   const pointColor = '#2FDBBC';
 
   const [priceRange, setPriceRange] = useState([1000, 50000]);
-  const [selectedTheme, setSelectedTheme] = useState();
-  const [selectedValue, setSelectedValue] = useState<number>(1);
-  const [additionalPickers, setAdditionalPickers] = useState<JSX.Element[]>([]);
-  const [activeButtons, setActiveButtons] = useState<string[]>([]);
-
-  // 버튼을 렌더링하기 위한 배열
-  const buttons = ['한식', '양식', '중식', '일식'];
-  const handleButtonPress = (value: string) => {
-    setActiveButtons(prevActiveButtons => {
-      if (prevActiveButtons.includes(value)) {
-        // 이미 선택된 버튼을 다시 클릭하면 선택 해제
-        return prevActiveButtons.filter(button => button !== value);
-      } else {
-        // 선택되지 않은 버튼을 클릭하면 선택
-        return [...prevActiveButtons, value];
-      }
-    });
-  };
-
-  // 선택된 버튼에 따라 스타일을 결정하는 함수
-  const getButtonStyle = (value: string) => {
-    return {
-      // 기본 스타일
-      padding: 10,
-      margin: 5,
-      borderWidth: 1,
-      borderColor: '#ccc',
-      // 선택된 버튼이면 배경색을 변경
-      backgroundColor: activeButtons.includes(value) ? 'blue' : 'white',
+  const [selectedTags, setSelectedTags] = useState<SelectedTag[]>([]);
+  
+  // 태그 선택
+  const selectTag = (tag: Tag) => {
+    // 태그 선택 시 현재 시각과 랜덤 값을 조합하여 고유 ID 생성
+    const newTag: SelectedTag = {
+      ...tag,
+      id: `${tag.key}_${Date.now()}_${Math.random().toString(36).substr(2, 9)}`,
     };
+    setSelectedTags((prevSelectedTags) => [...prevSelectedTags, newTag]);
   };
+  // 태그 삭제
+  const removeTag = (id: string) => {
+    setSelectedTags((prevSelectedTags) =>
+      prevSelectedTags.filter((tag) => tag.id !== id)
+    );
+  };
+
+  // 선택 가능한 태그 렌더링
+  const renderTag = ({ item }: { item: Tag }) => (
+    <TouchableOpacity style={styles.tag} onPress={() => selectTag(item)}>
+      <Image source={item.icon} style={styles.icon} />
+      <Text>{item.key}</Text>
+    </TouchableOpacity>
+  );
+
+  // 선택된 태그 렌더링
+  const renderSelectedTag = ({ item, index }: { item: SelectedTag; index: number }) => (
+    <View style={styles.selectedTag}>
+      <Image source={item.icon} style={styles.icon} />
+      <Text>{item.key}</Text>
+      <TouchableOpacity onPress={() => removeTag(item.id)}>
+        <Text style={styles.removeIcon}>x</Text>
+      </TouchableOpacity>
+    </View>
+  );
   const multiSliderValuesChange = (values: number[]) => setPriceRange(values);
-
-  const createPickers = (count: number) => {
-    const pickers: JSX.Element[] = [];
-    for (let i = 0; i < count; i++) {
-      pickers.push(
-        <Picker
-          key={i}
-          selectedValue={selectedValue}
-          style={styles.picker}
-          onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
-        >
-          <Picker.Item label="밥" value={1} />
-          <Picker.Item label="카페" value={2} />
-          <Picker.Item label="산책" value={3} />
-          <Picker.Item label="숙박" value={4} />
-          <Picker.Item label="기타활동" value={5} />
-          {/* Add more options as needed */}
-        </Picker>
-      );
-    }
-    setAdditionalPickers(pickers);
-  };
-
 
   return (
     <View style={tw`flex-1 bg-white px-4 py-4`}>
@@ -97,57 +89,23 @@ const Search: React.FC<SearchProps> = ({ navigation }) => {
         <Text style={tw`text-lg`}>{`${priceRange[0].toLocaleString()} 원`}</Text>
         <Text style={tw`text-lg`}>{`${priceRange[1].toLocaleString()} 원`}</Text>
       </View>
-      <View style={tw`mb-4 `}>
-        <Text style={tw`mb-2 text-gray-700`}>테마</Text>
-        <View style={[tw`h-10 px-4 rounded-full `, { backgroundColor: '#F4F4F4'}]}>
-          <Picker
-            selectedValue={selectedTheme}
-            onValueChange={(itemValue, itemIndex) =>
-              setSelectedTheme(itemValue)
-            }
-            style={tw`flex-1`}
-            dropdownIconColor="#A0AEC0"
-          >
-            {}
-            <Picker.Item label="" value="" />
-            <Picker.Item label="로맨틱" value="male" />
-            <Picker.Item label="활동적" value="female" />
-            <Picker.Item label="실내" value="other"/>
-          </Picker>
-        </View>
-      </View>
-      <Text style={tw`mb-2 text-gray-700`}>코스장소갯수</Text>
-      <View>
-      {/* Picker to select the number of additional pickers */}
+      <Text style={tw`mb-2 text-gray-700`}>데이트장소를 순서대로 선택해주세요</Text>
 
-      <Picker
-        selectedValue={selectedValue}
-        style={styles.picker}
-        onValueChange={(itemValue, itemIndex) => {
-          setSelectedValue(itemValue);
-          createPickers(itemValue);
-        }}
-      >
-        <Picker.Item label="1" value={1} />
-        <Picker.Item label="2" value={2} />
-        <Picker.Item label="3" value={3} />
-        <Picker.Item label="4" value={4} />
-        <Picker.Item label="5" value={5} />
-      </Picker>
-
-      {/* Render the additional pickers */}
-      <View style={styles.pickerContainer}>
-    {/* Render the additional pickers */}
-    {additionalPickers.map((picker) => picker)}
-  </View>
-      
-      <View style={tw`mb-4 items-center`}>
-        <Image
-          source={require('../assets/talk.png')}
-          style={{ width: 200, height: 200, resizeMode: 'contain' }}
+      <View style={styles.container}>
+        <FlatList
+          data={tagsData}
+          renderItem={renderTag}
+          keyExtractor={(item) => item.key}
+          horizontal
+        />
+        <FlatList
+          data={selectedTags}
+          renderItem={renderSelectedTag}
+          keyExtractor={(item, index) => index.toString()} // 여기를 수정했습니다.
+          horizontal
         />
       </View>
-    </View>
+
     <TouchableOpacity
         style={[tw`rounded-full py-2 mb-4`, { backgroundColor: pointColor }]}
         onPress={() => navigation.navigate("Search")}
@@ -162,6 +120,10 @@ const Search: React.FC<SearchProps> = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 50,
+  },
   selectedTrack: {
     backgroundColor: '#2FDBBC',
   },
@@ -188,6 +150,34 @@ const styles = StyleSheet.create({
   picker: {
     height: 50,
     flex: 1, // 가로로 균등하게 피커들을 배분
+  },
+  tag: {
+    margin: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  selectedTag: {
+    margin: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
+  icon: {
+    width: 20,
+    height: 20,
+    marginRight: 5,
+  },
+  removeIcon: {
+    marginLeft: 5,
+    color: 'red',
   },
 });
 
