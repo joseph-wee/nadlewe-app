d23e0f52367f69c14702447776c1df534a59570b
joseph <joseph@josephui-MacBookPro.local>
2023-11-22 15:05:23 +0900

    design: 폰트, 배경색 제외 상세페이지UI구현  Please enter the commit message for your changes. Lines starting

--- Pages/Detail.tsx
+++ Pages/Detail.tsx
@@ -1,11 +1,92 @@
-import { Text, View } from 'react-native';
+import { Image, Text, View } from "react-native";
 
 const Detail = () => {
   return (
-    <View>
-      <Text>Detail</Text>
+    <View style={{ padding: 20 }}>
+      <Text style={{ marginBottom: 15 }}>코스 소개</Text>
+      {/** 아이템 하나 */}
+      <View style={{ marginBottom: 30 }}>
+        <Text style={{ marginBottom: 10 }}>박지후 스시</Text>
+        <Image
+          source={require("../assets/food.png")}
+          style={{
+            marginBottom: 20,
+            width: "100%",
+            height: 230,
+            borderRadius: 20,
+          }}
+        />
+        <View
+          style={{
+            marginBottom: 12,
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <Text>2인 SET</Text>
+          <Text>39,000원</Text>
+        </View>
+        <Text>
+          스시2인세트, 샐러드, 초밥20pcs, 간장새우, 새우튀김, 뚝 배기, 우동
+        </Text>
+      </View>
+      {/** 아래코드는 보여주기용 나중에 api연동시 삭제 */}
+      <Text style={{ marginBottom: 15 }}>코스 소개</Text>
+      {/** 아이템 하나 */}
+      <View style={{ marginBottom: 30 }}>
+        <Text style={{ marginBottom: 10 }}>박지후 스시</Text>
+        <Image
+          source={require("../assets/food.png")}
+          style={{
+            marginBottom: 20,
+            width: "100%",
+            height: 230,
+            borderRadius: 20,
+          }}
+        />
+        <View
+          style={{
+            marginBottom: 12,
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <Text>2인 SET</Text>
+          <Text>39,000원</Text>
+        </View>
+        <Text>
+          스시2인세트, 샐러드, 초밥20pcs, 간장새우, 새우튀김, 뚝 배기, 우동
+        </Text>
+      </View>
+      <Text style={{ marginBottom: 15 }}>코스 소개</Text>
+      {/** 아이템 하나 */}
+      <View style={{ marginBottom: 30 }}>
+        <Text style={{ marginBottom: 10 }}>박지후 스시</Text>
+        <Image
+          source={require("../assets/food.png")}
+          style={{
+            marginBottom: 20,
+            width: "100%",
+            height: 230,
+            borderRadius: 20,
+          }}
+        />
+        <View
+          style={{
+            marginBottom: 12,
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <Text>2인 SET</Text>
+          <Text>39,000원</Text>
+        </View>
+        <Text>
+          스시2인세트, 샐러드, 초밥20pcs, 간장새우, 새우튀김, 뚝 배기, 우동
+        </Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Detail
+export default Detail;
