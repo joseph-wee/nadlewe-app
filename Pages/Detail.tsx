514600e5cfb508f00031e9a6406247541c9a759d
joseph <joseph@josephui-MacBookPro.local>
2023-12-05 20:39:55 +0900

    feat: 좋아요 기능 구현

--- Pages/Detail.tsx
+++ Pages/Detail.tsx
@@ -1,12 +1,24 @@
 import { useEffect, useState } from "react";
 import { Image, Text, View, ScrollView } from "react-native";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import likeList from "../likeData";
 
 const Detail = ({ route }: any) => {
   useEffect(() => {
-    console.log(route.params.courses[0].places);
+    console.log(route.params);
   }, []);
   // 임시 데이터
   const [detail, setDetail] = useState(route.params.courses[0].places);
+  const [isActive, setIsActive] = useState(false);
+
+  const [like, setLike] = useRecoilState<any>(likeList);
+  const currentLike = useRecoilValue(likeList);
+
+  const likeHandler = () => {
+    setIsActive(true);
+
+    setLike([...like, route.params]);
+  };
 
   return (
     <ScrollView style={{ backgroundColor: "white" }}>
@@ -14,6 +26,7 @@ const Detail = ({ route }: any) => {
         style={{ width: "100%", height: 95, resizeMode: "cover" }}
         source={require("../assets/ad_burger.png")}
       />
+
       <View style={{ padding: 20 }}>
         <View style={{ flex: 1, flexDirection: "row" }}>
           <Text
@@ -26,16 +39,22 @@ const Detail = ({ route }: any) => {
           >
             코스 소개
           </Text>
-          <Image
-            style={{
-              width: 20,
-              height: 20,
-              position: "absolute",
-              right: 0,
-              top: -2,
-            }}
-            source={require("../assets/icons/Heart.png")}
-          />
+          <View
+            onTouchEnd={likeHandler}
+            style={{ position: "absolute", right: 0, top: -2 }}
+          >
+            <Image
+              style={{
+                width: 20,
+                height: 20,
+              }}
+              source={
+                isActive
+                  ? require("../assets/icons/Heart.png")
+                  : require("../assets/icons/Heart_default.png")
+              }
+            />
+          </View>
         </View>
         {/** 아이템 하나 */}
         {detail.map((el: any, index: number) => {
